0e32e5ab26fdab455946cc8c30017dd5b0bb96eb
DiwakarT <diwakar.timrin@gmail.com>
2026-08-13 13:36:40 +0530

    fix vite base url for asset paths

--- vite.config.js
+++ vite.config.js
@@ -37,6 +37,9 @@ if (host === "localhost") {
 }
 
 export default defineConfig({
+    base: process.env.SHOPIFY_APP_URL
+    ? `${process.env.SHOPIFY_APP_URL}/`
+    : "/",
   server: {
     allowedHosts: [host],
     cors: {
